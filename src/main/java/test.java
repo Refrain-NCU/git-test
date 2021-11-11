4dc12e6346183a0dfa74f0f54b664766ead17161
hznb <784229045@qq.com>
2021-11-11 13:36:41 +0800

    third commit

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -3,5 +3,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("hello Git~~~~~");
         System.out.println("Hi,git~~~~~");
+        System.out.println("I like git~~~~~");
     }
 }
